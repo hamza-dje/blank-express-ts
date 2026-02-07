50b97380e9679964bcd2274d75ead8269d8ed46d
Hamza Djedidi <35818353+hamza-dje@users.noreply.github.com>
2026-02-07 17:24:17 +0100

    add http-status-codes package

--- src/controllers/appController.ts
+++ src/controllers/appController.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from "express";
 import { greetUserMessage } from "../services/appService";
+import { StatusCodes } from "http-status-codes";
 
 export const greetUser = (req: Request, res: Response) => {
     const message = greetUserMessage();
-    return res.status(200).json({ message });
+    return res.status(StatusCodes.OK).json({ message });
 };
